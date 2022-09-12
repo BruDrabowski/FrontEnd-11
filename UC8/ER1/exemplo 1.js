4a042295e41a3744a058089f0310eca708f62558
Bruna Drabowski <bm.drabowski@gmail.com>
2022-09-12 19:44:06 -0300

    primeira parte ER1 de logica

---
+++ UC8/ER1/exemplo 1.js
@@ -0,0 +1,17 @@
+//variavéis
+let num1=1;
+let num2=5;
+let resultado;
+
+//entrada
+num1=1
+num2=5
+
+//processamento
+resultado= num1 +num2
+
+//saída
+console.log();
+
+
+
---
+++ UC8/ER1/exemplo2.js
@@ -0,0 +1,15 @@
+//sistema para um radar de velocidade
+
+//variáveis
+let velocidade;
+
+//entrada
+velocidade = 100
+
+//processamento
+if(velocidade>100){
+    console.log("você foi multado")
+}
+else{
+    console.log ("você está dentro do limite da velocidade")
+}
