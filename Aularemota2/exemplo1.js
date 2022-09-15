906f9be5ca0808d8ec3f425ccafaf70cca010dfd
Bruna Drabowski <bm.drabowski@gmail.com>
2022-09-15 19:48:59 -0300

    aula2parte1

---
+++ Aularemota2/exemplo1.js
@@ -0,0 +1,12 @@
+//regras para operadores de programação
+/*
+= (é utilizado para atribuição)
+== (comparação-verifica se é igual)
+
+|| pipe
+
+*/
+let x = 10
+let y = 20
+let resultado = (x==y)
+console.log(resultado)
