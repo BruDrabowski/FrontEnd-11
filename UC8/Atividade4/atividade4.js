279b49b14912f08d685bb0f22e8703fc60b674a0
Bruna Drabowski <bm.drabowski@gmail.com>
2022-09-20 20:38:18 -0300

    fina atividade 4

--- UC8/Atividade4/atividade4.js
+++ UC8/Atividade4/atividade4.js
@@ -1,15 +1,8 @@
 //Algoritmo-cadastro de eventos//
 
-//declaração das variáveis//
 let idade = 27
-let listaDeParticipantes = []
-listaDeParticipantes.push('Mara Souza')
-listaDeParticipantes.push('Suzana Freitas')
-listaDeParticipantes.push('Cláudio Manuel')
-listaDeParticipantes.push('Augusto Flores')
-listaDeParticipantes.push('Luís Pereira')
     
-
+//condição da idade para cadastro
 
 if (idade >= 18) {
     console.log("cadastro permitido!")
@@ -18,16 +11,30 @@ else{
     console.log("cadastro não permitido pela idade!!!")
 }
 
-let númeroDeParticipantes = 102
 
-for (let contador = 1; contador <= númeroDeParticipantes ; contador++)
+//exibir lista de participantes
+
+for (let contador = 1; contador <= 100 ; contador++)
 {
     if (contador <= 100){
     console.log(`${contador}`)
-   
-    }
+       
+}   
 }
 
-else (contador > 100); {
-    console.log("limite de participantes excedido!")
-    }
+//condição de número de participantes para o cadastro
+
+let listaDeParticipantes = []
+listaDeParticipantes.push('Mara Souza')
+listaDeParticipantes.push('Suzana Freitas')
+listaDeParticipantes.push('Cláudio Manuel')
+listaDeParticipantes.push('Augusto Flores')
+listaDeParticipantes.push('Luís Pereira')
+
+if (listaDeParticipantes.length<101){
+    console.log("cadastro concluído!")
+}
+
+else {
+    console.log ("limite de participantes excedido!")
+}
---
+++ UC8/Atividade4/testes.js
@@ -0,0 +1,16 @@
+//declaração das variáveis//
+let idade = 27
+let listaDeParticipantes = []
+listaDeParticipantes.push('Mara Souza')
+listaDeParticipantes.push('Suzana Freitas')
+listaDeParticipantes.push('Cláudio Manuel')
+listaDeParticipantes.push('Augusto Flores')
+listaDeParticipantes.push('Luís Pereira')
+
+if (listaDeParticipantes.length<101){
+    console.log("cadastro concluído!")
+}
+
+else {
+    console.log ("limite de participantes excedido!")
+}
