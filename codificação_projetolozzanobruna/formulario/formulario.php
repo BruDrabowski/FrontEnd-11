8e58b426484bbec695f6c544d5f7f6459123f294
Bruna Drabowski <bm.drabowski@gmail.com>
2022-10-29 15:36:13 -0300

    formulario

---
+++ codificação_projetolozzanobruna/formulario/formulario.php
@@ -0,0 +1,6 @@
+<form>
+            <div class="form-group">
+                <label>Nome:</label>
+                <input type="text" name="nome" id="nome" placeholder="digite o seu nome">
+            </div>
+        </form>
